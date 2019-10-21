b9d61653c1d8a214e97ac63d0759c8755ebe8279
dwane richards <dwanerich@gmail.com>
2019-10-21 23:17:37 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
-first_number = a
-second_number = b
+first_number = 4
+second_number = 2
 
-sum = a + b
-difference = a - b
-product = a * b
-quotient = a / b
+sum = 4 + 2
+difference = 4 - 2
+product = 4 * 2
+quotient = 4 / 2
